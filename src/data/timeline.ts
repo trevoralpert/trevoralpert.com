c19599e8944dba3dd7014e10f6bfeaf65ca8500d
Trevor Alpert <trevoralpert1@gmail.com>
2025-06-10 21:54:20 -0400

    Relink About photos

--- src/data/timeline.ts
+++ src/data/timeline.ts
@@ -4,8 +4,8 @@ export interface TimelineEntry {
 }
 
 export const timeline: TimelineEntry[] = [
-  { images: ["/timeline/timeline-01.jpg"], caption: "I was born in New York in 1994." },
-  { images: ["/timeline/timeline-02.jpg"], caption: "My family moved to South Florida in 1995 so that was all I remembered." },
+  { images: ["/timeline/timeline-01.JPG"], caption: "I was born in New York in 1994." },
+  { images: ["/timeline/timeline-02.JPG"], caption: "My family moved to South Florida in 1995 so that was all I remembered." },
   { images: ["/timeline/timeline-03.jpg"], caption: "(2009) I had a normal Floridian childhood, spending most of my time outside playing sports." },
   { images: ["/timeline/timeline-04.jpg"], caption: "(2010) Occasionally taking advantage of the best beaches in the country." },
   { images: ["/timeline/timeline-05.jpg"], caption: "(2011) When I reached the age that sports became serious, I chose Lacrosse." },
@@ -13,18 +13,18 @@ export const timeline: TimelineEntry[] = [
   { images: ["/timeline/timeline-07.jpg"], caption: "(2012) My Coach, Doug Shanahan (right), was one of the best lacrosse players who ever lived. The first ever Tewaaraton Award Winner (The Heisman of Lacrosse) and the MVP of Team USA when they won the World Lacrosse Championship against Canada in 2002." },
   { images: ["/timeline/timeline-08.jpg", "/timeline/timeline-09.jpg"], caption: "He taught me a thing or two." },
   { images: ["/timeline/timeline-10.jpg"], caption: "I even got to play in a tournament at Sunlife Stadium, where the Miami Dolphins and the University of Miami Hurricanes played." },
-  { images: ["/timeline/timeline-11.jpg"], caption: "After high school I went to the University of Miami. Here I am in 2013 with the then President of the University, Donna Shalala, former United States Secretary of Health and Human Services under President Bill Clinton." },
+  { images: ["/timeline/timeline-11.JPG"], caption: "After high school I went to the University of Miami. Here I am in 2013 with the then President of the University, Donna Shalala, former United States Secretary of Health and Human Services under President Bill Clinton." },
   { images: ["/timeline/timeline-12.jpg"], caption: "(2015) Despite picking up on the usual bad habits in college I got into the best shape of my life." },
-  { images: ["/timeline/timeline-13.jpg"], caption: "Coach Shanahan who was always passionate about growing the sport of Lacrosse in South Florida, went on to start a brand new program at St. Brendan Catholic High School and he hired me to be his JV Head Coach while I was in college in Miami. It was a school for first generation immigrants from Cuba so the three of us coaches in this photo were the only faculty members who couldn't speak Spanish. The boys had a field day with that." },
+  { images: ["/timeline/timeline-13.JPG"], caption: "Coach Shanahan who was always passionate about growing the sport of Lacrosse in South Florida, went on to start a brand new program at St. Brendan Catholic High School and he hired me to be his JV Head Coach while I was in college in Miami. It was a school for first generation immigrants from Cuba so the three of us coaches in this photo were the only faculty members who couldn't speak Spanish. The boys had a field day with that." },
   { images: ["/timeline/timeline-14.jpg"], caption: "(2016) Of course all good things must come to an end." },
-  { images: ["/timeline/timeline-15.jpg"], caption: "(2017) But as one door closes another opens. I moved myself to Los Angeles on a hope and a prayer and ended up landing a job on an ABC Sitcom." },
-  { images: ["/timeline/timeline-16.jpg"], caption: "Here's my first ever on screen credit that aired on National Television in 2017." },
+  { images: ["/timeline/timeline-15.JPG"], caption: "(2017) But as one door closes another opens. I moved myself to Los Angeles on a hope and a prayer and ended up landing a job on an ABC Sitcom." },
+  { images: ["/timeline/timeline-16.JPG"], caption: "Here's my first ever on screen credit that aired on National Television in 2017." },
   { images: ["/timeline/timeline-17.jpg"], caption: "More proof that I worked on the show. Can you spot me? I doubt it." },
   { images: ["/timeline/timeline-18.PNG"], caption: "(2018) But it wasn't enough to work on someone else's show. I had the hunger to write my own. Thus the Web Series, Welcome to Weho, was born out of the minds of myself and my roommate Richie and our friend Morgan." },
   { images: ["/timeline/timeline-19.jpg"], caption: "Naturally we had to do some research on the subject matter. Here's us with some more friends at the West Hollywood pride parade." },
   { images: ["/timeline/timeline-20.jpg"], caption: "I continued to write and direct my own short films in my free time while working on American Housewife." },
   { images: ["/timeline/timeline-21.jpg"], caption: "Thus Mediform Productions was born." },
-  { images: ["/timeline/timeline-22.jpg", "/timeline/timeline-23.jpg", "/timeline/timeline-24.jpg"], caption: "Started by myself, Jeeraya 'Bill' Moranon, and Desiree Cardona (both pictured above)." },
+  { images: ["/timeline/timeline-22.jpg", "/timeline/timeline-23.JPG", "/timeline/timeline-24.jpg"], caption: "Started by myself, Jeeraya 'Bill' Moranon, and Desiree Cardona (both pictured above)." },
   { images: ["/timeline/timeline-25.jpg"], caption: "While shooting independent content I linked up with a talented Actor named Matt Kempner." },
   { images: ["/timeline/timeline-26.jpg"], caption: "He would star in many of my projects going forward." },
   { images: ["/timeline/timeline-27.jpeg"], caption: "(2020) A lot changed when the COVID-19 pandemic hit. I got laid off from ABC along with many others and started focusing on Mediform full time and Bill and I started a homemade hand sanitizer brand called Astro Moonbeam." },
@@ -51,6 +51,6 @@ export const timeline: TimelineEntry[] = [
   { images: ["/timeline/timeline-54.jpg"], caption: "Twice!" }
 ];
 
-export const finalTimelineImage = "/timeline/timeline-55.jpg";
+export const finalTimelineImage = "/timeline/timeline-55.JPG";
 
 export const finalTimelineText = "Rideshare came out in 2023 in the midst of the WGA Strike. Ever since then the entertainment industry has yet to bounce back to where it was when I first moved to LA when there was a lot more opportunity. Over the course of the next 2 years, I continued to work in service jobs to pay off the budget of the web series while promoting it. I was offered a few streaming deals from some up and coming streaming services but none of the offers were enough to cover the cost of production and they wanted to own the web series and the intellectual property. This was the point where I realized I had to make a change in my direction in life. I've been so fulfilled in my creative pursuits but it's become clear that in the current industry climate there's no way for someone to break in without already being established and with Artificial Intelligence on the horizon it doesn't seem like it's going to get any better in the long run. So I thought to myself where can I be creative and get onto the forefront of something new? Well the same thing that would've eventually taken my job if I stayed in entertainment: Artificial Intelligence."; 
